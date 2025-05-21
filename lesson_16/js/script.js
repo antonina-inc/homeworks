00da2c99e55c095b703ceb00c3e444c968779b79
antonina-inc <sharlota.st@gmail.com>
2025-05-21 22:53:31 +0300

    lesson_16

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -2,9 +2,9 @@ document.addEventListener('click', documentActions)
 
 function documentActions(e) {
 	const targetElement = e.target
-	if (targetElement.closest('.menu__arrow')) {
-		targetElement.closest('.menu__item').classList.toggle('active')
-	}
+	// if (targetElement.closest('.menu__item')) {
+	// 	targetElement.closest('.menu__item').classList.toggle('active')
+	// }
 	if (targetElement.closest('.icon-menu')) {
 		document.body.classList.toggle('menu-open')
 	}
